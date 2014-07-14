ee93af22eb43de07884efa53765568265635c3b7
Diego Marques <diegmarq@gmail.com>
2014-07-14 13:00:05 -0300

    Adicionado CRUD provisório para AcidenteSiar

--- siar/src/main/java/br/com/siar/controllers/AcidenteSiarController.java
+++ siar/src/main/java/br/com/siar/controllers/AcidenteSiarController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.View;
@@ -17,15 +18,29 @@ public class AcidenteSiarController {
 
 	@Autowired
 	private AcidenteSiarService acidenteSiarService;
+	
 	@RequestMapping(value = "/acidentesiar", method = RequestMethod.GET)
-	public String getCustomerList(ModelMap model) {
+	public String getAcidentesList(ModelMap model) {
 		model.addAttribute("acidenteSiarList", acidenteSiarService.listAcidentes());
 		return "acidentesiar";
 	}
+	
 	@RequestMapping(value = "/acidentesiar/save", method = RequestMethod.POST)
-	public View saveCustomer(@ModelAttribute AcidenteSiar acidenteSiar, ModelMap model) {
+	public View saveAcidente(@ModelAttribute AcidenteSiar acidenteSiar, ModelMap model) {
 		acidenteSiarService.saveAcidente(acidenteSiar);
 		return new RedirectView("/siar/acidentesiar");
+	}
+	
+	@RequestMapping(value = "/acidentesiar/delete/{id}", method = RequestMethod.GET)
+	public View removeAcidente(@PathVariable String id, ModelMap model) {
+		acidenteSiarService.removeAcidente(id);
+		return new RedirectView("/siar/acidentesiar");
+	}
+	
+	@RequestMapping(value = "/acidentesiar/updateacidente/{id}", method = RequestMethod.GET)
+	public String updateAcidente(@PathVariable String id, ModelMap model){
+		model.addAttribute("acidenteUpdate", acidenteSiarService.findAcidenteById(id));
+		return "updateacidente";
 	}
 	
 }
--- siar/src/main/webapp/WEB-INF/views/acidentesiar.jsp
+++ siar/src/main/webapp/WEB-INF/views/acidentesiar.jsp
@@ -6,18 +6,25 @@
         <form action="/siar/acidentesiar/save" method="post">
             <label for="descricao">Descrição</label>
             <input type="text" id="descricao" name="descricao"/>
-                        <label for="prioridade">Prioridade</label>
+            <label for="prioridade">Prioridade</label>
             <input type="text" id="prioridade" name="prioridade"/>
             
             <input type="submit" value="Submit"/>
         </form>
  
-    <table border="2">
+    <table border="1">
+        <tr>
+            <th>Descrição</th>
+            <th>Prioridade</th>
+            <th>Atualizar</th>
+            <th>Remover</th>
+        </tr>
         <c:forEach var="acidente" items="${acidenteSiarList}">
             <tr>
                 <td>${acidente.descricao}</td>
                 <td>${acidente.prioridade}</td>
-                
+                <td><a href="/siar/acidentesiar/updateacidente/${acidente.id}"><button>Update</button></a></td>
+                <td><a href="/siar/acidentesiar/delete/${acidente.id}"><button>Remove</button></a></td>
             </tr>
         </c:forEach>
     </table>  
